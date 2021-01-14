9d2d7753e7f4e5435f487183db5c99e78c6937dd
Daniel Gomes <danielgomesjr@gmail.com>
2021-01-14 00:24:38 -0300

    QrCode

---
+++ QrCode/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QrCode.ViewModels;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace QrCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string url)    
+        {
+            var viewModel = new IndexViewModel();
+            if (string.IsNullOrEmpty(url))
+            {
+                return View(viewModel);
+            }
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                ViewData["url"] = url;
+                ViewData["Msg"] = "The url is not valid.";
+                return View(viewModel);
+            }
+            var image = GenerateByteArray(url);
+            viewModel.ImageB64 = String.Format("data:image/png;base64,{0}", Convert.ToBase64String(image));
+            return View(viewModel);
+        }
+
+        public static Bitmap GenerateImage(string url)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+            return qrCodeImage;
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateImage(url);
+            return ImageToByte(image);
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+
+    }
+}
---
+++ QrCode/ViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrCode.ViewModels
+{
+    public class IndexViewModel
+    {
+        public string ImageB64 { get; set; }
+    }
+}
